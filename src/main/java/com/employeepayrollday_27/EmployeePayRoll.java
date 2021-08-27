9a648578b0e187b332583810d225b5af8ec23050
vismayatk002 <vismayatk002@gmail.com>
2021-08-27 22:36:16 +0530

    Refactor read and write the Employee Pay roll

--- src/main/java/com/employeepayrollday_27/EmployeePayRoll.java
+++ src/main/java/com/employeepayrollday_27/EmployeePayRoll.java
@@ -1,9 +1,26 @@
 package com.employeepayrollday_27;
 
 public class EmployeePayRoll {
+
+	int id, salary;
+	String name;
 	
-    public static void main( String[] args ) {
-    	
-        System.out.println( "Welcome to Employee Pay Roll" );
+	public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+    public int getSalary(){
+        return salary;
     }
 }
---
+++ src/main/java/com/employeepayrollday_27/EmployeePayRollMain.java
@@ -0,0 +1,40 @@
+package com.employeepayrollday_27;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeePayRollMain {
+	
+	ArrayList<EmployeePayRoll> empList = new ArrayList<>();
+	
+	public void readEmpPayRollData() {
+		
+		EmployeePayRoll emp = new EmployeePayRoll();
+		Scanner sc = new Scanner(System.in);
+		System.out.print( "Enter Employee Name : ");
+		String name = sc.nextLine();
+		emp.setName(name);
+		System.out.print( "Enter Employee Id : ");
+		int id = sc.nextInt();
+		emp.setId(id);
+		System.out.print( "Enter Employee Salary : ");
+		int salary = sc.nextInt();
+		emp.setSalary(salary);
+		empList.add(emp);
+	}
+	public void writeEmpPayRollData() {
+		
+		for(EmployeePayRoll emp : empList) {
+			
+			System.out.print( "\nEmployee Name : " + emp.getName());
+			System.out.print( "\nEmployee Id : " + emp.getId());
+			System.out.print( "\nEmployee Salary : " + emp.getSalary());
+		}
+	}
+    public static void main( String[] args ) {
+    	
+    	EmployeePayRollMain payRoll = new EmployeePayRollMain();
+    	payRoll.readEmpPayRollData();
+    	payRoll.writeEmpPayRollData();
+    }
+}
