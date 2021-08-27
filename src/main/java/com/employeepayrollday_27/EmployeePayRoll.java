896e72dcd8d3f5afd268b6ab7cadab6fcf47d1aa
vismayatk002 <vismayatk002@gmail.com>
2021-08-27 14:09:25 +0530

    Added welcome message

---
+++ src/main/java/com/employeepayrollday_27/EmployeePayRoll.java
@@ -0,0 +1,9 @@
+package com.employeepayrollday_27;
+
+public class EmployeePayRoll {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "Welcome to Employee Pay Roll" );
+    }
+}
